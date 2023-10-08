239fab14ae9424aff085d59e8764127d95a996f4
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-10-08 16:49:55 +0300

    Pushed changes

---
+++ CSharp_Course/TodoList/TodoList/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("[S]ee all TODOs");
+            Console.WriteLine("[A]dd a TODO");
+            Console.WriteLine("[R]emove a TODO");
+            Console.WriteLine("[E]xit");
+
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("User input " + userChoice);
+            userChoice = "ABC";
+
+            Console.ReadKey();
+        }
+    }
+}
