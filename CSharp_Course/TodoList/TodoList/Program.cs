44a8c98d154cb3cbf165e7eefc2bbbbb5a6399f5
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-10-10 18:34:53 +0300

    Finished a part of the While loop

--- CSharp_Course/TodoList/TodoList/Program.cs
+++ CSharp_Course/TodoList/TodoList/Program.cs
@@ -4,36 +4,103 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("[S]ee all TODOs");
-            Console.WriteLine("[A]dd a TODO");
-            Console.WriteLine("[R]emove a TODO");
-            Console.WriteLine("[E]xit");
+            Console.WriteLine("Enter a word");
+            var userInput = Console.ReadLine();
 
-            var userChoise = Console.ReadLine().ToLower();
+            while (userInput.Length < 15)
+            {
+                userInput += 'a';
+                Console.WriteLine(userInput);
+            }
+            Console.WriteLine("The loop is finished");
 
-            if (userChoise == "S")
-            {
-                PrintSelectedOption("See al TODOs");
-            }
-            else if (userChoise == "A") 
-            {
-                PrintSelectedOption("Add a TODO");
-            }
-            else if (userChoise == "R")
-            {
-                PrintSelectedOption("Remove a TODO");
-            }
-            else if (userChoise == "E")
-            {
-                PrintSelectedOption("Exit");
-            }
+
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("[S]ee all TODOs");
+            //Console.WriteLine("[A]dd a TODO");
+            //Console.WriteLine("[R]emove a TODO");
+            //Console.WriteLine("[E]xit");
+
+            //var userChoise = Console.ReadLine().ToUpper();
+
+            //switch(userChoise)
+            //{
+            //    case "S":
+            //        PrintSelectedOption("See all TODOs");
+            //        break;
+            //    case "A":
+            //        PrintSelectedOption("Add a TODO");
+            //        break;
+            //    case "R":
+            //        PrintSelectedOption("Remove a TODO");
+            //        break;
+            //    case "E":
+            //        PrintSelectedOption("Exit");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid choice");
+            //        break;
+            //}
+
+
+
+            //if (userChoise == "S")
+            //{
+            //    PrintSelectedOption("See all TODOs");
+            //}
+            //else if (userChoise == "A") 
+            //{
+            //    PrintSelectedOption("Add a TODO");
+            //}
+            //else if (userChoise == "R")
+            //{
+            //    PrintSelectedOption("Remove a TODO");
+            //}
+            //else if (userChoise == "E")
+            //{
+            //    PrintSelectedOption("Exit");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid choise");
+            //}
 
             Console.ReadKey();
 
             void PrintSelectedOption (string selectedOption)
             {
                 Console.WriteLine("Selected option: " + selectedOption);
+            }
+
+            char ConvertPointsToGrade (int points)
+            {
+                char grade;
+                if (points == 10 || points == 9) 
+                {
+                    grade = 'A';
+                }
+                else if (points <=8 && points >= 6)
+                {
+                    grade = 'B';
+                }
+                else if (points <=5 && points >= 3)
+                {
+                    grade = 'C';
+                }
+                else if(points == 2 || points == 1)
+                {
+                    grade = 'D';
+                }
+                else if (points == 0)
+                {
+                    grade = 'E';
+                }
+                else
+                {
+                    grade = '!';
+                }
+
+                return grade;
             }
         }
     }
