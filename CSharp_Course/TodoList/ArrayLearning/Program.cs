d459a763625f3490b2fab87b4f8b8f67da0259cc
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-10-14 22:49:30 +0300

    learned multy-dimensional arrays

---
+++ CSharp_Course/TodoList/ArrayLearning/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.InteropServices;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 6, 7, 17, 22 };
+            var firstFromEnd = numbers[^1]; // the same as we use      = numbers[numbers.Length -1];     to show the last number from the array
+            var secondFromEnd = numbers[^2];
+
+            var sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            foreach (int i in numbers) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Sum of the elements is: {sum}");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp_Course/TodoList/Multy-dimensional arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Multy_dimensional_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] letters = new char[2,3];
+
+            letters[0, 0] = 'A';
+            letters[0, 1] = 'B';
+            letters[0, 2] = 'C';
+            letters[1, 0] = 'D';
+            letters[1, 1] = 'E';
+            letters[1, 2] = 'F';
+
+            // how we print dimensional array
+            var height = letters.GetLength(0);
+            var width = letters.GetLength(1);
+
+            Console.WriteLine($"Height is: {height}");
+            Console.WriteLine($"Width is: {width}");
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine($"i is: {i}");
+
+                for (int j = 0; j < width; j++)
+                {
+                    Console.WriteLine($"j is: {j}");
+                    Console.WriteLine($"element is: {letters[i,j]}");
+                }
+            }
+
+            // another way to declare two-dimensional array
+
+            var letters2 = new char[,]
+            {
+                {'A', 'B', 'C'},
+                { 'D', 'E', 'F'}
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
