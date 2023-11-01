31734928fbde2460820b877c2a099a6344a2a639
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-11-01 21:37:57 +0200

    OOP Learning

---
+++ CSharp_Course/OOP Learning/DataTimeLearning/Program.cs
@@ -0,0 +1,8 @@
+﻿DateTime internationalPizzaDay23 = new DateTime(2023, 11, 1);
+
+Console.WriteLine($"Year is {internationalPizzaDay23.Year}");
+Console.WriteLine($"Month is {internationalPizzaDay23.Month}");
+Console.WriteLine($"Day is {internationalPizzaDay23.Day}");
+Console.WriteLine($"The dy of the week is {internationalPizzaDay23.DayOfWeek}");
+
+Console.ReadKey();
---
+++ CSharp_Course/OOP Learning/OOP Learning/Program.cs
@@ -0,0 +1,25 @@
+﻿Rectangle rectangle1 = new Rectangle(5, 10);
+
+Console.WriteLine($"Width is {rectangle1.Width}");
+Console.WriteLine($"Width is {rectangle1.Height}");
+
+Rectangle rectangle2 = new Rectangle(12, 3);
+
+Console.WriteLine($"Width is {rectangle2.Width}");
+Console.WriteLine($"Width is {rectangle2.Height}");
+
+Console.ReadKey();
+
+class Rectangle
+{
+    // fields
+    public int Width;
+    public int Height;
+
+    //constructor
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
