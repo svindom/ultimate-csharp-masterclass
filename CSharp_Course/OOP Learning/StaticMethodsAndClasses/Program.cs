3c99c1e15b4e7ee22edb1bab7fb4cb6e41a03e9b
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-11-10 17:16:34 +0200

    Learned about static class and method

---
+++ CSharp_Course/OOP Learning/StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine($"1 + 2 is {Calculator.Add(1,2)}");
+Console.WriteLine($"1 - 2 is {Calculator.Subtract(1, 2)}");
+Console.WriteLine($"1 * 2 is {Calculator.Multiply(1, 2)}");
+
+
+Console.ReadLine();
+
+// Static class can only contain static methods.
+// Hovewer, non-static class can contain static methods.
+static class Calculator
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+    public static int Multiply(int a, int b) => a * b;
+}
