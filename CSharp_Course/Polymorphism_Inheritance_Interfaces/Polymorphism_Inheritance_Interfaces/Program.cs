db47ccd96961635436d8c17cb4363e744e9f4194
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-11-22 22:39:58 +0200

    Learning Inheriting

---
+++ CSharp_Course/Polymorphism_Inheritance_Interfaces/Polymorphism_Inheritance_Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿Pizza pizza = new Pizza();
+pizza.AddIngrediaents(new Chedar());
+pizza.AddIngrediaents(new Mozarella());
+pizza.AddIngrediaents(new TomatoSause());
+
+Console.WriteLine(pizza.Describe());
+Console.ReadKey();
+
+public class Pizza
+{
+    private List<Ingredient> _ingredients = new List<Ingredient>();
+
+    public void AddIngrediaents (Ingredient ingredient) =>    // check what lambda operator is.
+    _ingredients.Add(ingredient);
+    
+    public string Describe ()
+    {
+        return $"This is a pizza with {string.Join(", ", _ingredients)}";
+    }
+}
+
+
+public class Ingredient
+{
+
+}
+
+public class Chedar : Ingredient
+{
+    // properties
+    public string Name  // public string Name => "Cheddar cheese";
+    {
+        get
+        {
+            return "Cheddar cheese";
+        }
+    }
+    public int AgedForMonth { get; }
+}
+
+public class TomatoSause : Ingredient
+{
+    // properties
+    public string Name => "Tomato sause";
+    public int TomatosIn100Grams { get; }
+}
+
+public class Mozarella : Ingredient
+{
+    // properties
+    public string Name => "Mozarella";
+    public bool isLight { get; }
+}
