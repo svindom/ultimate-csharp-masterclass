2186d635772032e70ce4ce16080c002ff7d587c9
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-11-23 18:45:36 +0200

    Learning virtual methods

---
+++ CSharp_Course/Polymorphism_Inheritance_Interfaces/OverridingMembersFromClass/Program.cs
@@ -0,0 +1,87 @@
+﻿
+//Console.WriteLine("Variable of type cheddar");
+//Cheddar cheddar = new Cheddar();
+//Console.WriteLine(cheddar.Name);
+
+//Console.WriteLine("Variable of type Ingredient");
+//Ingredient ingredient = new Cheddar();
+//Console.WriteLine(ingredient.Name);
+
+List<Ingredient> ingredients = new List<Ingredient>
+{
+    new Cheddar(),
+    new Mozarella(),
+    new TomatoSause()
+};
+
+foreach(Ingredient ingredient in ingredients)
+{
+    Console.WriteLine(ingredient.Name);
+}
+
+
+
+Console.ReadKey();
+
+public class Pizza
+{
+    private List<Ingredient> _ingredients = new List<Ingredient>();
+
+    public void AddIngrediaents(Ingredient ingredient) =>    // check what lambda operator is.
+    _ingredients.Add(ingredient);
+
+    public string Describe()
+    {
+        return $"This is a pizza with {string.Join(", ", _ingredients)}";
+    }
+}
+
+
+public class Ingredient
+{
+    //PROPERTIES
+    public virtual string Name { get; } = "Some ingredient";
+
+    //fields
+    public int PublicField; // This field is the same as it is in each of derived classes but we can declare different values in each of new instances
+
+    // Methods
+    public string PublicMethod() =>
+        "This method is PUBLIC in the Ingredient class.";
+    private string PrivateMethod() =>
+        "This method is PRIVATE in the Ingredient class.";
+    protected string ProtectedMethod() =>
+        "This method is PROTECTED in the Ingredient class."; // I can access the protected method in a derived class but NOT outside else
+}
+
+public class Cheddar : Ingredient
+{
+    // properties
+    public override string Name  // public string Name => "Cheddar cheese";
+    {
+        get
+        {
+            return "Cheddar cheese";
+        }
+    }
+    public int AgedForMonth { get; }
+
+    public void UseMethodsFromBaseClass()
+    {
+        Console.WriteLine(PublicMethod());
+    }
+}
+
+public class TomatoSause : Ingredient
+{
+    // properties
+    public override string Name => "Tomato sause";
+    public int TomatosIn100Grams { get; }
+}
+
+public class Mozarella : Ingredient
+{
+    // properties
+    public override string Name => "Mozarella";
+    public bool isLight { get; }
+}
---
+++ CSharp_Course/Polymorphism_Inheritance_Interfaces/VirtualMethods Practise/Program.cs
@@ -0,0 +1,44 @@
+﻿List<int> numbers = new List<int> { 1, 4, 6 - 1, 12, 44, -8, -19 };
+bool shallAddPositiveOnly = true;
+
+int sum;
+if (shallAddPositiveOnly)
+{
+    sum = new PositiveNumberSumCalculator().Calculator(numbers);
+}
+else
+{
+    sum = new NumberSumCalculator().Calculator(numbers);
+}
+
+Console.WriteLine($"Sum is: {sum}");
+
+Console.ReadKey();
+
+public class NumberSumCalculator
+{
+    public int Calculator(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            if (ShallBeAdded(number))
+            {
+                sum += number;
+            }
+        }
+        return sum;
+    }
+    protected virtual bool ShallBeAdded (int number)
+    {
+        return true;
+    }
+}
+
+public class PositiveNumberSumCalculator : NumberSumCalculator
+{
+    protected override bool ShallBeAdded(int number)
+    {
+        return number > 0;
+    }
+}
