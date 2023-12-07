c29750970945532c1966654b572c2e5ed5feefa6
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-12-07 21:17:18 +0200

    Learning interfaces in C#

---
+++ CSharp_Course/Polymorphism_Inheritance_Interfaces/Animal/Program.cs
@@ -0,0 +1,40 @@
+﻿public interface IFlyable
+{
+    void Fly();
+}
+public class Bird : IFlyable
+{
+    public void Tweet()
+    {
+        Console.WriteLine("Tweet, tweet");
+    }
+    public void Fly() 
+    {
+        Console.WriteLine("Flying using its winds");
+    }
+}
+
+public class Kite : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("Flying carried by the wind");
+    }
+}
+
+public interface IFuelable
+{
+    void Fuel();
+}
+
+public class Plane : IFlyable, IFuelable
+{
+    public void Fly()
+    {
+        Console.WriteLine("Flying using jet propulsion");
+    }
+    public void Fuel()
+    {
+        Console.WriteLine("Filling tanks with jet fuel");
+    }
+}
---
+++ CSharp_Course/Polymorphism_Inheritance_Interfaces/InterfacesExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿public static class Exercise
+{
+    public static int Transform(
+        int number)
+    {
+        var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+
+        var result = number;
+        foreach (var transformation in transformations)
+        {
+            result = transformation.Transform(result);
+        }
+        return result;
+    }
+}
+
+public interface INumericTransformation
+{
+    int Transform (int number);
+}
+
+public class By1Incrementer : INumericTransformation
+{
+    public int Transform(int number)
+    {
+        return number + 1;
+    }
+}
+
+public class By2Multiplier : INumericTransformation
+{
+    public int Transform(int number)
+    {
+        return number * 2;
+    }
+}
+
+public class ToPowerOf2Raiser : INumericTransformation
+{
+    public int Transform (int number)
+    {
+        return number * number;
+    }
+}
