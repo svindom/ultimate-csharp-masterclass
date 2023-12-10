ca32ded63d0942e2e078254c1c6b7540007ea2d0
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-12-10 22:11:02 +0200

    Start working on a new CookisCookbok application

---
+++ CSharp_Course/CookiesCookbook/CookiesCookbook/Program.cs
@@ -0,0 +1,74 @@
+﻿CookiesRecipesApp cookiesRecipesApp = new CookiesRecipesApp( new RecipesRepository(), new RecipesConsoleUserInteraction());
+cookiesRecipesApp.Run();
+
+Console.ReadKey();
+
+public class CookiesRecipesApp
+{
+    private readonly IRecipesRepository _recipesRepository;
+    private readonly IRecipesUserInteraction _recipesUserInteraction;
+
+    public CookiesRecipesApp(IRecipesRepository recipesRepository, IRecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    public void Run()
+    {
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+        _recipesUserInteraction.PromtToCreateRecipe();
+
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+
+        if (ingredients.Count > 0)
+        {
+            var recipes = new Recipe(ingredients);
+            allRecipes.Add(recipe);
+            _recipesRepository.Write(filePath, allRecipes);
+
+            _recipesUserInteraction.ShowMessage("Recipe added:");
+            _recipesUserInteraction.ShowMessage(recipes.ToString());
+        }
+        else
+        {
+            _recipesUserInteraction.ShowMessage("No ingredients have been selected. " + "Recipe will not be saved.");
+        }
+
+        _recipesUserInteraction.Exit();
+    }
+}
+
+public interface IRecipesUserInteraction
+{
+    public void ShowMessage(string message);
+    public void Exit();
+
+}
+
+public class RecipesConsoleUserInteraction : IRecipesUserInteraction
+{
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to close the app.");
+        Console.ReadKey();
+    }
+}
+
+
+public interface IRecipesRepository
+{
+
+}
+
+public class RecipesRepository : IRecipesRepository
+{
+}
---
+++ CSharp_Course/CookiesCookbook/CookiesCookbook/Recipes/Recipe.cs
@@ -0,0 +1,18 @@
+﻿namespace CookiesCookbook.Recipes
+{
+    public class Recipe
+    {
+        public IEnumerable<Ingredient> Ingredients { get; }
+
+        public Recipe(IEnumerable<Ingredient> ingredients) 
+        {
+            Ingredients = ingredients;
+        }
+
+    }
+
+    public abstract class Ingredient
+    {
+
+    }
+}
