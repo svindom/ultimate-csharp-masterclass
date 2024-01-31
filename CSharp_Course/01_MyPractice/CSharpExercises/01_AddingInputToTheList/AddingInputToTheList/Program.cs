61058410b893a509d1c34e90c7a0632b1eb8716b
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-01-31 16:49:34 +0200

    Worked on an exercise of adding values from the input to the list

---
+++ CSharp_Course/01_MyPractice/CSharpExercises/01_AddingInputToTheList/AddingInputToTheList/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace AddingInputToTheList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            List<string> wordsList = new List<string>();
+            WordsAdder wordsAdder = new WordsAdder();
+
+            wordsAdder.AddWordsToList(wordsList);
+            wordsAdder.DisplayWords(wordsList);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class WordsAdder
+    {
+        public WordsAdder()
+        {
+        }
+
+        public void AddWordsToList(List<string> wordsList) 
+        {
+            GetDescription();
+            // Split the input to the array of words
+            string userInput = Console.ReadLine();
+            string[] words = userInput.Split(',');
+
+            foreach (string word in words) 
+            {
+                // Trim to remove any leading or trailing spaces
+                wordsList.Add(word.Trim());
+            }
+
+        }
+
+        public void DisplayWords(List<string> wordsList)
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("You have added the following words to the List:");
+            int wordsCounter = 0;
+            foreach (string word in wordsList)
+            {
+                wordsCounter++;
+                Console.WriteLine($"{wordsCounter}. {word}");
+            }
+        }
+
+        public void GetDescription()
+        {
+            Console.WriteLine("Please enter words separated by commas that you want to add to the list:");
+        }
+    }
+}
+
