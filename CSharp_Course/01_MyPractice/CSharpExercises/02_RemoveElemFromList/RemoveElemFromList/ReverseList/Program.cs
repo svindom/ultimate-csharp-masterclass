ba510ce853a75caf8dec7cc5a1f99126ee111f43
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-02 21:22:08 +0200

    Reversed elements from the list

---
+++ CSharp_Course/01_MyPractice/CSharpExercises/02_RemoveElemFromList/RemoveElemFromList/ReverseList/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ReverseList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // how to reverse List numbers manually
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> reverseNumbers = new List<int>();
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                reverseNumbers.Add(numbers[i]);
+            }
+
+            Console.WriteLine("Numbers in the list are:");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Reversed numbers are: ");
+            foreach (int number in reverseNumbers) 
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            // Another more simple way to reverse List elements
+
+            List<int> numbersList = new List<int>() { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+            numbersList.Reverse();
+            foreach (int number in numbersList)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
