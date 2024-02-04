647f155f119ffcdda4b2748303e9718d11daf1c4
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-04 18:57:19 +0200

    Was learning recursion with C#

---
+++ CSharp_Course/01_MyPractice/CSharpExercises/RecursionEexercise/Recursion/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        // Recursion occurs whe a method is called within itself
+        // Factorial-5 = 5*4*3*2*1
+        static void Main(string[] args)
+        {
+            int valueInt = 3;
+            int factorialResult = Factoraial(valueInt);
+            Console.WriteLine($"Factorial answer of {valueInt} is: {factorialResult}");
+
+            Console.WriteLine();
+
+            float valueFloat = 2f;
+            int powerOf = 4;
+            float powerResult = Power(valueFloat, powerOf);
+            Console.WriteLine($"Power answer of {valueFloat} is: {powerResult}");
+
+
+
+            Console.ReadKey();
+        }
+
+        private static int Factoraial(int factorialNum)
+        {
+            // EXIT CONDITION HERE
+            if (factorialNum == 1)   
+            {
+                return 1;
+            }
+            return factorialNum * Factoraial(factorialNum - 1);
+        }
+
+        private static float Power(float num, int powerOf)
+        {
+            // EXIT CONDITION HERE
+            if (powerOf == 1)
+            {
+                return num;
+            }
+
+            return num * Power(num, powerOf - 1);
+        }
+    }
+}
---
+++ CSharp_Course/01_MyPractice/CSharpExercises/RecursionEexercise/RecursionEexercise/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace RecursionEexercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Itteration recursion = new Itteration();
+
+            int positiveNumber = recursion.GetPositiveNumber();
+            recursion.PrintTheResult(positiveNumber);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Itteration
+    {
+        public int GetPositiveNumber()
+        {
+            int positiveNumber = 0; // Initialize positeveNumber with a default value
+            while (true) // Use an infinite loop that we'll break out of on valid input
+            {
+                Console.WriteLine("Enter any positive natural number which will be printed:");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out positiveNumber) && positiveNumber > 0)
+                {
+                    return positiveNumber; // Return and barake a loop immediatelly upon receiving valid input
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Enter a positive number");
+                    Console.WriteLine();
+                }
+            }
+            // No need for return staetment here because the loop cannot exit without returning a valid number
+        }
+        public void PrintTheResult(int positiveNumber)
+        {
+            Console.WriteLine();
+            for (int i = 1; i <= positiveNumber; i++) // Start from 1 to match the user input expectation
+            {
+                if (i < positiveNumber) // For numbers before the last one, add a comma and space
+                {
+                    Console.Write(i + ", "); 
+                }
+                else // For the last number, just print the number without a comma and space
+                {
+                    Console.Write(i); 
+                }
+            }
+        }
+    }
+}
