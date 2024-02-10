a495962d82da34c78ad13c02da000f9c14266880
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-10 16:40:42 +0200

    Did toString exercise

---
+++ CSharp_Course/01_MyPractice/CSharpExercises/ToStringExercise/ToStringExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ToStringExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+
+            for (int i = 0; i < total; i++ )
+            {
+                Console.WriteLine("Enter a person name:");
+                persons[i] = new Person() { Name = Console.ReadLine() };
+            }
+            Console.WriteLine("---------------------------------------------");
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hi my name is {Name}!";
+        }
+    }
+}
