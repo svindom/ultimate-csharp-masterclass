53d3f89a69d6e8e4a6254094a44f5881ad534727
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-02-12 19:41:11 +0200

    Add C# exercise to practice inheritance concepts.

---
+++ CSharp_Course/01_MyPractice/CSharpExercises/PhotoBookExercise/PhotoBookExercise/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace PhotoBookExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBook photoBookDefault = new PhotoBook(24);
+            BigPhotoAlbum bigPhotoAlbum = new BigPhotoAlbum();
+            SmallPhotoAlbum smallPhotoAlbum = new SmallPhotoAlbum();
+
+            Console.WriteLine($"The Default Photo book has {photoBookDefault.NumPages} pages.");
+            Console.WriteLine($"The Big Photo book has {bigPhotoAlbum.NumPages} pages.");
+            Console.WriteLine($"The Small Photo book has {smallPhotoAlbum.NumPages} pages.");
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class PhotoBook
+    {
+        private int _numPages;
+
+        public int NumPages
+        {
+            get
+            {
+                return _numPages;
+            }
+            private set
+            {
+                _numPages = value;
+            }
+        }
+
+        public PhotoBook()
+        {
+            _numPages = 16;
+        }
+
+        public PhotoBook(int numPages) 
+        {
+            _numPages = numPages;
+        }
+    }
+
+    public class BigPhotoAlbum : PhotoBook
+    {
+        public BigPhotoAlbum() : base(64)
+        {
+
+        }
+    }
+    public class SmallPhotoAlbum : PhotoBook
+    {
+        public SmallPhotoAlbum() : base(16)
+        {
+
+        }
+    }
+
+}
---
+++ CSharp_Course/01_MyPractice/CSharpExercises/PhotoBookExercise/VirtualExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace VirtualExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(123.4);
+            Rectangle rectangle = new Rectangle(234, 78.5);
+
+            Console.WriteLine($"Circle with radius {circle.Radius} has an area of {circle.CalculateArea()}");
+            Console.WriteLine($"Rectangle with length {rectangle.Length} and width {rectangle.Width} has an area of {rectangle.CalculateArea()}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Shape
+    {
+        public virtual double CalculateArea()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Shape 
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+
+}
